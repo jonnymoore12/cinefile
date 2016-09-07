bae6dfad53e02afec7314e1d403ce5eca316562c
Harry Jaggs <harryjaggs92@gmail.com>
2016-09-07 18:11:35 +0100

    adding delete films

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -13,6 +13,15 @@ class FilmsController < ApplicationController
     redirect_to user_cinefile_path(@user.id, @cinefile.id)
   end
 
+  def destroy
+    @user = current_user
+    @cinefile = Cinefile.find(params[:cinefile_id])
+    @film = Film.find(params[:id])
+    @film.destroy
+    flash[:notice] = 'Film deleted successfully'
+    redirect_to user_cinefile_path(@user.id, @cinefile.id)
+  end
+
   private
 
   def film_params
--- spec/features/films_feature_spec.rb
+++ spec/features/films_feature_spec.rb
@@ -10,3 +10,16 @@ feature 'films' do
     expect(page).to have_content("Indiana Jones")
   end
 end
+
+context 'deleting films' do
+  scenario 'allows you to remove a film from your cinefile' do
+    sign_up
+    click_link 'Cinefile'
+    click_button "Add a film"
+    fill_in "Title", with: "Indiana Jones"
+    click_button "Add to Cinefile"
+    click_button 'Delete Indiana Jones'
+    expect(page).not_to have_content "Indiana Jones"
+    expect(page).to have_content "Film deleted successfully"
+  end
+end
