e6610674939e5a5b6a95fc0ffa89aa02c0a3a14e
Jonny Moore <jonnymoore121212@gmail.com>
2016-09-07 18:15:43 +0100

    Refactors CRUD tests

--- spec/features/films_feature_spec.rb
+++ spec/features/films_feature_spec.rb
@@ -3,10 +3,7 @@ require 'rails_helper'
 feature 'films' do
   scenario 'allows users to add films to their cinefile' do
     sign_up
-    click_link 'Cinefile'
-    click_button "Add a film"
-    fill_in "Title", with: "Indiana Jones"
-    click_button "Add to Cinefile"
+    add_film
     expect(page).to have_content("Indiana Jones")
   end
 end
@@ -14,10 +11,7 @@ end
 context 'deleting films' do
   scenario 'allows you to remove a film from your cinefile' do
     sign_up
-    click_link 'Cinefile'
-    click_button "Add a film"
-    fill_in "Title", with: "Indiana Jones"
-    click_button "Add to Cinefile"
+    add_film
     click_button 'Delete Indiana Jones'
     expect(page).not_to have_content "Indiana Jones"
     expect(page).to have_content "Film deleted successfully"
