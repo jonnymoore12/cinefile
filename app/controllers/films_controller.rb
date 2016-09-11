f256e108cc538165f8d57a04c6d8265f39ad89c8
JulieLev <jkgee1@yahoo.co.uk>
2016-09-11 16:57:32 +0100

    user can add a film to cinefile by using local film id

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,19 +4,12 @@ class FilmsController < ApplicationController
   end
 
   def new
-  #   @user = current_user
-  #   @cinefile = Cinefile.find(params[:cinefile_id])
     @film = Film.new
   end
   #
   def create
     @film = Film.create(film_params)
-   #@film = Film.create(film_params)
     redirect_to '/'
-  #   @user = current_user
-  #   @cinefile = Cinefile.find(params[:cinefile_id])
-  #   @user.cinefile.films.create(film_params)
-  #   redirect_to user_cinefile_path(@user.id, @cinefile.id)
   end
 
   def destroy
@@ -24,17 +17,9 @@ class FilmsController < ApplicationController
     @film.delete
     redirect_to films_path
   end
-  # def destroy
-  #   @user = current_user
-  #   @cinefile = Cinefile.find(params[:cinefile_id])
-  #   @film = Film.find(params[:id])
-  #   @film.destroy
-  #   flash[:notice] = 'Film deleted successfully'
-  #   redirect_to user_cinefile_path(@user.id, @cinefile.id)
-  # end
-  #
-  # private
-  #
+
+private
+
   def film_params
     params.require(:film).permit(:title, :tmdb_id)
     # Obviously this needs fixing!
@@ -42,7 +27,6 @@ class FilmsController < ApplicationController
     # params[:film_record] = params[:film_record].split(",")
     # Need code to define this (see total params hash below!)
   end
-
 end
 
 # Parameters: {"utf8"=>"✓", "authenticity_token"=>"OCpf2bcXha65yNZIpNPU1iqBgb5W+QZdwunSuJoz3ZlXZv3LtkcSDQZQBYTP9OsRucmrgUJePYMsqH0JIhKdHw==", "film_record"=>"Batman: The Killing Joke,2016,382322", "commit"=>"Create Film"}
--- spec/features/cinefiles_feature_spec.rb
+++ spec/features/cinefiles_feature_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 feature "Cinefiles" do
   context "User signed in" do
-
-    scenario 'A Cinefile link is available to signed in users' do
+    before do
+      add_film_temp
       sign_up
       click_link_cinefile
+    end
+
+    scenario 'A Cinefile link is available to signed in users' do
       expect(page).to have_content "Welcome to test's Cinefile"
     end
-  end
 
+    scenario "A user can add a film to their cinefile list" do
+        click_link "Add Film"
+        films = Film.all
+        filmid = films.last.id
+        fill_in "Local film id", with: filmid
+        click_button "Add Id"
+        expect(page).to have_content("Jaws")
+    end
+  end
 end
