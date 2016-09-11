1f743e804f1b551ce94d2bd48a35807d8326eca2
JulieLev <jkgee1@yahoo.co.uk>
2016-09-12 00:32:08 +0100

    adds validation for tmdb_id

--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -1,10 +1,3 @@
-# describe Film, type: :model do
-#   it 'has a title' do
-#     film = Film.new(title: "Jaws")
-#     expect(film.title).to eq "Jaws"
-#   end
-# end
-
 require 'rails_helper'
 
 describe Film, type: :model do
