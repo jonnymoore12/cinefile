b12ddf20921062b5ff4924da5eea8b8d43e5ea3b
JulieLev <jkgee1@yahoo.co.uk>
2016-09-12 00:35:13 +0100

    test for validation tmdb_id

--- spec/models/film_spec.rb
+++ spec/models/film_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Film, type: :model do
   it { is_expected.to have_many :list_films }
+
+  it "is not valid unless it has a unique tmdb_id" do
+    Film.create(title: "Midnight Oil", tmdb_id: "300")
+    film = Film.new(title: "Midnight Gas", tmdb_id: "300")
+    expect(film).to have(1).error_on(:tmdb_id)
+  end
 end
