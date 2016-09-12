e5b10d94745be3e676627e092d2b9d1dcb25d203
Adam <adamjbrowner@gmail.com>
2016-09-12 16:44:00 +0100

    Adding films now works regardless of commas in title

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -9,9 +9,9 @@ class FilmsController < ApplicationController
     @cinefile = @user.cinefile
     cinefile_id = @cinefile.id
     @film_info = params["film_record"].split(",")
-    @film_release_year = @film_info[1]
-    @tmdb_id = @film_info[2]
-    @title = @film_info[0]
+    @film_release_year = @film_info.delete(@film_info[-2])
+    @tmdb_id = @film_info.delete(@film_info[-1])
+    @title = @film_info.join(',')
     film_not_in_database = Film.find_by(tmdb_id: @tmdb_id).nil?
     if film_not_in_database
       Film.create(title: @title, tmdb_id: @tmdb_id, release_year: @film_release_year)
