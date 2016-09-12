889ff936c073e5b0596294980fb100f127090303
Harry Jaggs <harryjaggs92@gmail.com>
2016-09-12 11:19:26 +0100

    tests changed to pass with new styling

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -1,7 +1,7 @@
 feature "#Homepages" do
-  it "There is a homepage welcoming users to Cinefile" do
-    sign_up
+  it "There is a homepage for Cinefile where a user can sign up" do
     visit '/'
-    expect(page).to have_content("Welcome to Cinefile!")
+    expect(page).to have_content('Cinefile')
+    expect(page).to have_content('SIGN UP')
   end
 end
