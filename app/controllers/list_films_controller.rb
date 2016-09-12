734bf480202dafcf81477cc70401aca1d668e267
Adam <adamjbrowner@gmail.com>
2016-09-12 11:59:17 +0100

    Creat films and list_films in progress

--- app/controllers/list_films_controller.rb
+++ app/controllers/list_films_controller.rb
@@ -1,23 +1,10 @@
 class ListFilmsController < ApplicationController
 
-  def new
-    @user = current_user
-    @cinefile = @user.cinefile
-    @list_film = ListFilm.new
+  def index
+    redirect_to method: 'create'
   end
 
   def create
-    @user = current_user
-    @cinefile = @user.cinefile
-
-    @cinefile.list_films.create(list_film_params)
-    redirect_to user_cinefile_path(@user.id, @cinefile.id)
-  end
-
-  def list_film_params
-
-    params.require(:list_film).permit(:film_id)
-
   end
 
 end
