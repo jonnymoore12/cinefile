86d025d7a5c19ac77dab469132c81553eeb7d8b5
JulieLev <jkgee1@yahoo.co.uk>
2016-09-12 02:42:27 +0100

    check if film exists when adding list_film

--- app/controllers/list_films_controller.rb
+++ app/controllers/list_films_controller.rb
@@ -8,17 +8,21 @@ class ListFilmsController < ApplicationController
   end
 
   def create
+    #byebug
     @user = current_user
     @cinefile = @user.cinefile
-
-    @cinefile.list_films.create(list_film_params)
+    ref_id = params[:list_film]['film_id'].to_i
+    
+    if Film.exists?(ref_id)
+      # Film.exists?(tmdb_id: ref_tmdb)
+      @cinefile.list_films.create(film_id: ref_id)
+    else
+      @cinefile.list_films.create(list_film_params)
+    end
     redirect_to user_cinefile_path(@user.id, @cinefile.id)
   end
 
   def list_film_params
-
     params.require(:list_film).permit(:film_id)
-
   end
-
 end
